914fcce630274269e86b4638ff1d988257a7a7b7
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-04-30 23:33:12 +0200

    added link for cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#16a34a]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://drive.google.com/file/d/1k4kLeQSeLyT3rhMnO8h-4YBQWIQpaSbD/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
